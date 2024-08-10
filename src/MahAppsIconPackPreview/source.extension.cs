7c5ae9ed55961d60726f2fc57c633be2a078797e
davqq <davidvongarrel10219@gmail.com>
2024-08-11 00:12:50 +0200

    fix description [release]

--- src/MahAppsIconPackPreview/source.extension.cs
+++ src/MahAppsIconPackPreview/source.extension.cs
@@ -2,7 +2,7 @@
     internal sealed partial class Vsix {
         public const string Id = "MahAppsIconPackPreview.4ec776a2-0d38-402b-adc9-9508413af6b0";
         public const string Name = "MahApps.IconPack Preview Extension";
-        public const string Description = @"This Visual Studio extension provides a preview of MahApps icons directly in the C# code editor. When you reference a MahApps icon in your code, this extension displays a visual preview of the icon next to the code reference, making it easier to find and use the correct icons.";
+        public const string Description = @"Enhance your development with the MahApps.IconPack Preview Extension for Visual Studio Code. Instantly preview MahApps.Metro.IconPacks icons directly in your code, ensuring accuracy and efficiency. Simplify your workflow with real-time icon visualization.";
         public const string Language = "en-US";
         public const string Version = "1.0.2";
         public const string Author = "davq";
