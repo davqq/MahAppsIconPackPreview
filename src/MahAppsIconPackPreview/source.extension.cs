952090be3d774a7f6c03fa0aaa5367e6fd43dd21
davqq <davidvongarrel10219@gmail.com>
2024-08-10 22:07:58 +0200

    add logo

--- src/MahAppsIconPackPreview/source.extension.cs
+++ src/MahAppsIconPackPreview/source.extension.cs
@@ -1,11 +1,11 @@
 ﻿namespace MaterialIconPreview {
     internal sealed partial class Vsix {
         public const string Id = "MahAppsIconPackPreview.4ec776a2-0d38-402b-adc9-9508413af6b0";
-        public const string Name = "MahApps Icon Pack Preview";
+        public const string Name = "MahApps.IconPack Preview Extension";
         public const string Description = @"This Visual Studio extension provides a preview of MahApps icons directly in the C# code editor. When you reference a MahApps icon in your code, this extension displays a visual preview of the icon next to the code reference, making it easier to find and use the correct icons.";
         public const string Language = "en-US";
-        public const string Version = "1.0.1";
-        public const string Author = "davqq";
+        public const string Version = "1.0.2";
+        public const string Author = "davq";
         public const string Tags = "MahApps, Icon, Preview";
     }
 }
